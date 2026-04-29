ad6ad3bace36361a86a22f056c5ccecffd324eba
Deepti Chouhan <deepti.chouhan@encoresky.com>
2026-04-29 17:52:11 +0530

    Removed --headed for CI compatibility

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False) # Launch browser in non-headless mode (visible)
+        browser = p.chromium.launch(headless=True) # Launch browser in non-headless mode (visible)
         page = browser.new_page()  # Create new page
         page.goto("https://hrms.eznity.ai")
         yield page
